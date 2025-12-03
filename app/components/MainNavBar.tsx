4020abdccd57f35ab432ce08d160b33e8f044638
JasubiPL <gameportal7@hotmail.com>
2025-12-03 17:49:10 -0600

    fix(navbar): ajustar tamaños de iconos y texto en la barra de navegación

--- app/components/MainNavBar.tsx
+++ app/components/MainNavBar.tsx
@@ -9,11 +9,11 @@ export const MainNavBar = ({ previusPage, nextPage, title }: { previusPage: stri
     <nav className="w-full sticky top-0 bg-white z-1 ">
       <div className="p-4 flex justify-between items-center border-b-2 border-gray-100">
         <Link href={previusPage}>
-          <MdOutlineKeyboardArrowLeft className="text-5xl fill-red-700 bg-gray-100 rounded-full"/>
+          <MdOutlineKeyboardArrowLeft className="text-3xl fill-red-700 bg-gray-100 rounded-full"/>
         </Link>
-        <p className="text-xl">{title}</p>
+        <p className="text-lg">{title}</p>
           <Link href={nextPage}>
-          <MdOutlineKeyboardArrowRight className="text-5xl fill-red-700 bg-gray-100 rounded-full"/>
+          <MdOutlineKeyboardArrowRight className="text-3xl fill-red-700 bg-gray-100 rounded-full"/>
         </Link>
       </div>
     </nav>
