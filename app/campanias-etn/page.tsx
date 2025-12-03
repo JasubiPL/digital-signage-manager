3687966c2d7682ab50213f6fcb6d645793bad3c5
JasubiPL <gameportal7@hotmail.com>
2025-12-03 12:35:06 -0600

    feat(navigation): integrate navigation bar and update layout structure

---
+++ app/campanias-etn/page.tsx
@@ -0,0 +1,9 @@
+import RootLayout from "../layout";
+
+export default function EtnCampaignsPage() {
+  return (
+    <RootLayout>
+      <div>Etn Campaigns Page</div>
+    </RootLayout>
+  );
+}
--- app/components/MainNavBar.tsx
+++ app/components/MainNavBar.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+import { 
+  MdOutlineKeyboardArrowRight, 
+  MdOutlineKeyboardArrowLeft 
+} from "react-icons/md";
+
 export const MainNavBar = () => {
   return (
-    <nav className="w-full font-bold bg-red-700 p-4 text-white">
-      <p>Digital Signage Manager</p>
+    <nav className="w-full">
+      <div className="p-4 flex justify-between items-center border-b-2 border-gray-100">
+        <MdOutlineKeyboardArrowLeft className="text-5xl fill-red-700 bg-gray-100 rounded-full"/>
+        <p className="font-medium">Campañas publicitarias ETN</p>
+        <MdOutlineKeyboardArrowRight className="text-5xl fill-red-700 bg-gray-100 rounded-full"/>
+      </div>
     </nav>
   );
 };
